8d997105667b977a7d5e00aa630d503f8967fcb9
Evgeniy <zxcpapa00@gmail.com>
2024-05-27 22:44:06 +0200

    config for db

--- src/db/db.py
+++ src/db/db.py
@@ -1,12 +1,11 @@
-from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from src.config import settings
 
 
-DATABASE_URL = f'postgresql+asyncpg://{settings.TEST_DB_USER}:{settings.TEST_DB_PASS}@{settings.TEST_DB_HOST}:'\
-               f'{settings.TEST_DB_PORT}/{settings.TEST_DB_NAME}'
+DATABASE_URL = f'postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:'\
+               f'{settings.DB_PORT}/{settings.DB_NAME}'
 
 engine = create_async_engine(DATABASE_URL)
 
